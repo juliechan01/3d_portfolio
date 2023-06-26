89ccc6f2d45767ec020376e89bec391abe17b3fc
Julie <117595656+juliechan01@users.noreply.github.com>
2023-06-26 11:40:29 -0700

    Update Stars.jsx

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -2,7 +2,6 @@ import { useState, useRef, Suspense } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Preload } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
-import { positions } from '@mui/system';
 
 const Stars = (props) => {
   const ref = useRef();
