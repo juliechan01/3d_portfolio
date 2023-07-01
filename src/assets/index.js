cbc0dd6c912d080e5419a41bffb17fde5fc1073f
Julie <117595656+juliechan01@users.noreply.github.com>
2023-06-30 19:41:43 -0700

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,9 @@ import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
+import linkedin from "./linkedin.png";
+import hackerrank from "./hackerrank.png";
+import leetcode from "./leetcode.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -38,6 +41,9 @@ export {
   creator,
   mobile,
   web,
+  hackerrank,
+  leetcode,
+  linkedin,
   github,
   menu,
   close,
